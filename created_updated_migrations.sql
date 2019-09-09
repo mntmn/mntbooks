59bb94ce796efa6b50c8486fc84b2e0c21607356
mntmn <lukas@mntmn.com>
2019-09-09 15:36:47 +0200

    add sql migration for created/updated columns

---
+++ created_updated_migrations.sql
@@ -0,0 +1,6 @@
+alter table book add column created_at varchar(32);
+alter table book add column updated_at varchar(32);
+alter table documents add column created_at varchar(32);
+alter table documents add column updated_at varchar(32);
+alter table invoices add column created_at varchar(32);
+alter table invoices add column updated_at varchar(32);
